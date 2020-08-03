d095869ee5d7a72df45a1c940aba67dc10ba7490
Krishna Reddy <pkrishna.cap@outlook.com>
2020-08-03 20:02:12 +0530

    3aug

---
+++ datatype1.py
@@ -0,0 +1,11 @@
+a=True
+print(a)
+print(type(a))
+print(id(a))
+
+print("**********************")
+Order_Name="Samsung TV"
+print(Order_Name)
+print(type(Order_Name))
+print(id(Order_Name))
+
--- q.py
+++ q.py
@@ -1,3 +1,13 @@
-#
+a=10
+print(type(a))
 
-_student=
+a="Narasimha"
+print(type(a))
+
+
+a=True
+print(type(a))
+
+#print()
+#type()
+#id()
