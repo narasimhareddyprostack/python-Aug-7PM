410b3e99a7443ba69423cf59fc55a12ecc245583
Krishna Reddy <pkrishna.cap@outlook.com>
2020-08-10 20:17:09 +0530

    op

---
+++ Operators/op1.py
@@ -0,0 +1,10 @@
+print(10/2)    #  float  - 5.0
+print(10.5/2)  # float  - 5.25
+
+print(10//2)   #  #int 5   
+
+print(10.5//2.5)   # float  5.0
+print(10.5//2.5)   # float
+
+
+
---
+++ op2.py
@@ -0,0 +1,7 @@
+print("Prostack"+'10')
+
+print('Pro Stack' * 10.5 )
+
+print(10 * 'Pro Stack'  )
+
+# if your using *, one operanad should be integer
