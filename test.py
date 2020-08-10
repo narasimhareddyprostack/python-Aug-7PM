67a55e0ff2f81a1c21df2ae1750ddcf1f9855bae
Krishna Reddy <pkrishna.cap@outlook.com>
2020-08-10 19:37:56 +0530

    replacement

---
+++ test.py
@@ -0,0 +1,13 @@
+
+employee_no = 100
+employee_name="Krishna"
+employee_salary=20000
+loc="Hyd"
+
+print('Hello, My self {3} , My Emp_no {2} and salary: {1} , and worklocation is {0}'.
+                                        format(loc,employee_salary,employee_no,employee_name))
+ #                                               0    1                    2      3                              
+
+#{}
+#{}  digit  , follow indexing
+#{} with variable
