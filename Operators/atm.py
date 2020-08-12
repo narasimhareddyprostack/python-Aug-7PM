71cd9876ad6de3742b3daf30a46c21e8e76bd95b
Krishna Reddy <pkrishna.cap@outlook.com>
2020-08-12 18:51:30 +0530

    test

---
+++ Operators/atm.py
@@ -0,0 +1,6 @@
+pin_Number = int(input("Please Type/ Enter Pin Number: "))
+if( pin_Number == 5577):
+    print("Valid User/Account")
+else:
+    print("Please Enter Correct Password")
+
---
+++ Operators/log1.py
@@ -0,0 +1,14 @@
+#boolean
+
+# non - boolean Type
+'''
+a = 10
+b = 20
+c  = 30
+if(a < b and b > c ):
+     print("Valid")
+else:
+    print("Invalid")
+'''
+print(10 and "Hi")
+print('' and 'hi')
