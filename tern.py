1c39c5f61d1625a96d3689e3f7f6256bc4701294
Krishna Reddy <pkrishna.cap@outlook.com>
2020-08-17 20:12:47 +0530

    op

---
+++ tern.py
@@ -0,0 +1,25 @@
+a = 20
+b = 30
+c = 40
+d = 50
+
+e = 100 if  200>10 else 15
+
+Syntax:
+  First Operands if conditon else  second operator
+
+print(e)
+
+
+#f = a + b
+#f +=a  # f = f + a
+
+
+# int a = (a>b)? 40 : 50
+# First Operands if conditon else  second operator
+
+elif 
+if
+else
+
+    pass
